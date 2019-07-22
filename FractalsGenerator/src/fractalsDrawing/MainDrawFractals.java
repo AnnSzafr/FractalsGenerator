4133f4b410871b95a3130a20faa355c3f5acfcca
Anna Szafrańska <annszafr@gmail.com>
2019-07-22 23:10:03 +0200

    Creation methods with separate functionalities.
    Isolating the scene building class

--- FractalsGenerator/src/fractalsDrawing/MainDrawFractals.java
+++ FractalsGenerator/src/fractalsDrawing/MainDrawFractals.java
@@ -11,29 +11,56 @@ import javafx.scene.Scene;
 
 public class MainDrawFractals extends Application{
 	
-	public static void main(String[] args) {
-		launch();
+	final int primaryStageWidth = 950;
+	final int primaryStageHeight = 700;
+	String stageTitle = new String("FRACTAL DRAWING");
+	
+	private void setPrimaryStageSize(Stage stage) {
+		stage.setWidth(primaryStageWidth);
+		stage.setHeight(primaryStageHeight);
+	}
+	
+	private void setTitle(Stage stage) {
+		stage.setTitle(stageTitle);
 	}
 	
 	@Override
 	public void start(Stage primaryStage) {
 		long startTime = System.currentTimeMillis();
 		
-		primaryStage.setWidth(950);
-		primaryStage.setHeight(700);
-		
-		FractalChoice fractalChoice = new FractalChoice(primaryStage.getWidth(),primaryStage.getHeight());
-		Scene scene = new Scene(fractalChoice.mainPanel);
-		
-		fractalChoice.mainPanel.prefWidthProperty().bind(primaryStage.widthProperty());
-		fractalChoice.mainPanel.prefHeightProperty().bind(primaryStage.heightProperty());
+		setPrimaryStageSize(primaryStage);
+		setTitle(primaryStage);
 		
 		System.out.println(System.currentTimeMillis() - startTime);
-		primaryStage.setTitle("FRACTAL DRAWING");
-		primaryStage.setScene(scene);
+		
+		CreateScene createScene = new CreateScene(primaryStage);
+		createScene.create();
 		primaryStage.show();
 		
 		System.out.println("Koniec: "+(System.currentTimeMillis() - startTime));
 	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+	
+}
 
+class CreateScene {
+	private Stage stage;
+	FractalChoice fractalChoice;
+	
+	CreateScene(Stage stage) {
+		this.stage = stage;
+	}
+	private void scenePaneSettings() {
+		fractalChoice = new FractalChoice(stage.getWidth(),stage.getHeight()); 
+		fractalChoice.mainPanel.prefWidthProperty().bind(stage.widthProperty());
+		fractalChoice.mainPanel.prefHeightProperty().bind(stage.heightProperty());
+	}
+	public void create() {
+		scenePaneSettings();
+		Scene scene = new Scene(fractalChoice.mainPanel);
+		stage.setScene(scene);
+	}
 }
